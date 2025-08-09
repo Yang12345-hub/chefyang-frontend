84c9536d35a2869415a5aabcaa51e13a8fd0aa92
Yang Zhang <yangzhang@MacBookPro.attlocal.net>
2025-08-08 21:48:53 -0700

    Add cart handlers in data service.

--- src/services/menu.js
+++ src/services/menu.js
@@ -4,6 +4,14 @@ class MenuDataService {
     getMenu() {
         return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/chefyang/menu`);
     }
+
+    getCart(userId) {
+        return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/chefyang/cart/${userId}`);
+    }
+
+    upsertCart(data) {
+        return axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/v1/chefyang/cart`, data);
+    }
 }
 
 export default new MenuDataService();
